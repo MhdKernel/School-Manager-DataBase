97207979242b136b15f239633ba509c8cecabb73
Mohamad Mahdi Kazemi <mmkazemi99mahdi@gmail.com>
2025-07-15 22:44:11 +0330

    add script to run all data base

---
+++ init_all.sql
@@ -0,0 +1,7 @@
+-- ================================================
+-- Master script to initialize full database schema
+-- ================================================
+
+@schema/01_create_sequences.sql
+@schema/02_create_tables.sql
+@schema/03_create_triggers.sql
